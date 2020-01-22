9015b0cd0bb4a88a7d8bcc6e2686f8684cc333df
sjc <s.cole.j@gmail.com>
2020-01-22 11:53:39 +1100

    wip

--- src/RK4.hs
+++ src/RK4.hs
@@ -2,41 +2,59 @@ module RK4 where
 
 import Vec
 
-rk4stepv2 ::
-     [Vec2]
-  -> (Double -> [Vec2] -> [Vec2])
-  -> Double
-  -> [Vec2]
-rk4stepv2 ys f h =
-  foldl (zipWith addv2) (repeat zerov2) $
-     [ys] ++ (map (map (scalev2 (h / 6))) [f1, map (scalev2 2) f2, map (scalev2 2) f3, f4])
-  where
-    f1, f2, f3, f4 :: [Vec2]
-    f1 = f 0 ys
-    f2 = f (h / 2) (zipWith addv2 ys (map (scalev2 (h / 2)) f1))
-    f3 = f (h / 2) (zipWith addv2 ys (map (scalev2 (h / 2)) f2))
-    f4 = f h (zipWith addv2 ys (map (scalev2 h) f3))
+-- System variables
+-- Vec3, Vec2, Double
+-- just need (* :: Double -> b -> b) and (+ :: b -> b -> b)
+-- Is this num?
+-- Then the problems are:
+--   - if we use a list, they can be different lengths
+--   - list must have homogenous elements, so need a sum type of state var types
 
-rk4StepBody2 ::
-     Double
-  -> Body
-  -> Body
-rk4StepBody2 t b =
-  let [p', v'] = rk4stepv2 [bodyPos b, bodyVel b] (bodyAcc b) t
-  in b { bodyPos = p'
-       , bodyVel = v'
-       }
+-- | Aircraft state variables
+data AcSystem =
+  AcSystem { acTime :: Double -- ^ Absolute time
+           , acPos :: Vec3 -- ^ Position in 3D space
+           , acVel :: Vec2 -- ^ 2D velocity, longitudinal & vertical
+           , acMass :: Double -- ^ Mass of aircraft
+           }
+
+-- | Aircraft rate of change variables
+data AcRate =
+  AcRate { acrTime :: Double -- ^ Rate of change of absolute time; most likely equal 1
+         , acrVel :: Vec2 -- ^ 2D velocity, longitudinal & vertical
+         , acrAcc :: Vec2 -- ^ 2D acceleration, longitudinal & vertical
+         , acrMass :: Double -- ^ Rate of change of mass
+         }
+
+computeAcRates :: Double -> AcSystem -> AcRate
+computeAcRates dt s =
+  AcRate { acrTime = acTime s + dt
+         , acrVel = acVel s
+         , acrAcc = foldl addv2 zerov2 [weight, lift, drag, thrust]
+         , acrMass = 0
+         }
+  where weight = Vec2 0 (-9.81 * acMass s)
+        lift = Vec2 0 0
+        drag = Vec2 0 0
+        thrust = Vec2 0 0
+
+rk4step ::
+     (b -> a -> a) -- ^ How to add the rate of change type and the state type
+  -> (Double -> a -> b) -- ^ How to compute the instantaneous rate of change of the system.
+  -> (Double -> b -> b) -- ^ How to scale a rate of change
+  -> (b -> b -> b) -- ^ How to add rates of change
+  -> Double -- ^ Time step
+  -> a -- ^ Initial state
+  -> a -- ^ Next state
+rk4step sa f rs ra dt s0 = sa (rs (dt / 6) r) s0
+  where r = foldl1 ra [f1, rs 2 f2, rs 2 f3, f4]
+        f1 = f 0 s0
+        f2 = f (dt / 2) (sa (rs (dt / 2) f1) s0)
+        f3 = f (dt / 2) (sa (rs (dt / 2) f2) s0)
+        f4 = f dt (sa (rs dt f3) s0)
 
 type Pos2 = Vec2
 
 type Vel2 = Vec2
 
 type Acc2 = Vec2
-
-data Body =
-  Body
-    { bodyPos :: Vec2
-    , bodyVel :: Vec2
-    , bodyMass :: Double
-    , bodyAcc :: Double -> [Vec2] -> [Vec2]
-    }
