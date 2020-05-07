3f9b96242d32d8feaa43f5a6e821e1e95d410dbc
sjc <s.cole.j@gmail.com>
2020-05-07 19:52:32 +1000

    icon

--- web/js.js
+++ web/js.js
@@ -1,3 +1,27 @@
+var icon = new ol.Feature({
+    geometry: new ol.geom.Point(ol.proj.fromLonLat([145.365335, -37.698329])),
+});
+
+var iconStyle = new ol.style.Style({
+    image: new ol.style.Icon({
+        anchor: [0.5, 0.1],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'fraction',
+        src: './plane.png'
+    })
+});
+icon.setStyle(iconStyle);
+
+var vectorSource = new ol.source.Vector({
+  features: [icon]
+});
+
+var vectorLayer = new ol.layer.Vector({
+  source: vectorSource
+});
+
+/////
+
 var map = new ol.Map({
     layers: [
         new ol.layer.Tile({
@@ -6,7 +30,8 @@ var map = new ol.Map({
                 // FIXME terms of use?
                 url: "https://tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png"
             })
-        })
+        }),
+        vectorLayer
     ],
     target: 'map',
     view: new ol.View({
@@ -38,12 +63,17 @@ function setAltitude(ft) {
     setRotation(svg.getElementById("thousands"), ts)
 }
 
+var i = 0;
 window.addEventListener("load", function() {
+
     setAirspeed(0);
     setAltitude(0);
 
+    var mapRot = 0;
+
     var s = new WebSocket("ws://127.0.0.1:8000", "protocolOne");
     s.onmessage = function (event) {
+        i += 1;
         var j = JSON.parse(event.data);
         setAltitude(j.rAltitude)
         setAirspeed(j.rAirspeed)
@@ -51,17 +81,38 @@ window.addEventListener("load", function() {
         // FIXME would be great if these adjustments could be a bit smoother
         // maybe have a little plane which jumps
         // but the map smoothly chases the plane
-        map.getView().setCenter(ol.proj.fromLonLat([j.rLatLon[1], j.rLatLon[0]]));
-        var z = 14 - 3 / 10000 * j.rAltitude;
-        map.getView().setZoom(z);
-        map.getView().setRotation(-j.rHeadingRad);
+        var pos = ol.proj.fromLonLat([j.rLatLon[1], j.rLatLon[0]])
+
+
+        if (i % 15 == 0) {
+            mapRot = -j.rHeadingRad;
+            var z = 14 - 3 / 10000 * j.rAltitude;
+
+            map.getView().setCenter(pos);
+            map.getView().setZoom(z);
+            map.getView().setRotation(mapRot);
+
+            // map.getView().animate({
+            //     center: pos,
+            //     rotation: mapRot,
+            //     zoom: z,
+            //     duration: 1000
+            // });
+        }
+
+        icon.setGeometry(new ol.geom.Point(pos));
+        iconStyle.getImage().setRotation(mapRot + j.rHeadingRad);
+
+        // FIXME should allow tapping on map for pitch & heading
     }
 
     document.addEventListener("keydown", function(event) {
         if (event.key === "ArrowLeft") {
             s.send("l5");
+            i = 0;
         }
         if (event.key === "ArrowRight") {
+            i = 0;
             s.send("r5");
         }
         if (event.key === "ArrowUp") {
